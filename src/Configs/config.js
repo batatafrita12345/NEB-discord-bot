6abc190f6044999f1b249c793350d1bfe5fd10ef
batatafrita12345 <pedroguidop@hotmail.com>
2024-08-16 22:12:27 -0300

    first commit

--- src/Configs/config.js
+++ src/Configs/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-  GUILD_ID: "1274070375768129556",
-  BOT_ID: "1274068421427986516",
+  GUILD_ID: process.env.GUILD_ID,
+  BOT_ID: process.env.BOT_ID,
   TOKEN: process.env.CLIENT_TOKEN,
 };
